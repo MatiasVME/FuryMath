e94224352226c687450d9126a40c6d90a5fc9059
Matías Muñoz Espinoza <writkas2@gmail.com>
2016-04-19 23:57:37 -0300

    Mejor splash y menu

--- scripts/splash_screen.gd
+++ scripts/splash_screen.gd
@@ -3,9 +3,12 @@ extends Node2D
 
 var TOTAL_TIME = 3
 var accumulated_time = 0
+var accumulated_opacity = 1
 
 func _process(delta):
 	accumulated_time += delta
+	accumulated_opacity -= 0.25 * delta
+	get_node("splash").set_opacity(accumulated_opacity)
 	
 	if (TOTAL_TIME < accumulated_time):
 		get_tree().change_scene("res://scenes/menu_screen.tscn")
