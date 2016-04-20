bd5f95adaad7530123a84b82fc21b7ccd454974d
Matías Muñoz Espinoza <writkas2@gmail.com>
2016-04-20 03:27:09 -0300

    Animacion en el game menu, gracias a Seba Leiva

---
+++ scripts/botones.gd
@@ -0,0 +1,23 @@
+
+extends Node2D
+
+var rotation = 0
+var suma = null
+var multiplicacion = null
+var resta = null
+var division = null
+	
+func _process(delta):
+	rotation += 1 * delta
+	suma.set_rotate(rotation)
+	resta.set_rot(rotation)
+	multiplicacion.set_rot(rotation)
+	division.set_rot(rotation)
+
+func _ready():
+	set_process(true)
+	suma = get_node("s_suma")
+	resta = get_node("s_resta")
+	multiplicacion = get_node("s_multiplicacion")
+	division = get_node("s_division")
+
--- scripts/game_menu_screen.gd
+++ scripts/game_menu_screen.gd
@@ -2,13 +2,21 @@
 extends Node2D
 
 var rotation = 0
+var suma = null
 
 func _process(delta):
-	rotation += 0.01 * delta
+	#rotation += 0.01 
 	#get_node("suma").
-	get_node("s_suma").rotate(rotation)
+	#get_node("s_suma").rotate(rotation)
+	pass
+	
+func _fixed_process(delta):
+	rotation += 1 * delta
+	suma.set_rot(rotation)
+
 
 func _ready():
-	set_process(true)
+	set_fixed_process(true)
+	suma = get_node("s_suma")
 
 
