a630ff56bc4f0ae012020afccd086a4cf1da6942
Matías Muñoz Espinoza <writkas2@gmail.com>
2016-04-25 22:16:47 -0300

    algunos cambios

--- scripts/global.gd
+++ scripts/global.gd
@@ -16,5 +16,11 @@ var num_range_max = 6
 var num_range_min = 1
 
 # Progress bar
-var current_progress = 100
 var lost_time = 1
+
+func reset_values():
+	score = 0
+	current_level = 1
+	num_range_max = 6
+	num_range_min = 1
+	lost_time = 1
